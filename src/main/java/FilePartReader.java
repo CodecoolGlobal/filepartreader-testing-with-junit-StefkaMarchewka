7e0f5446a0c995af0c640eceabe87cc85c2f5ba6
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-07-30 09:45:27 +0200

    filePArtREader implemented according to requirements

--- src/main/java/FilePartReader.java
+++ src/main/java/FilePartReader.java
@@ -2,9 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
 
 public class FilePartReader {
     String filePath;
@@ -18,27 +16,36 @@ public class FilePartReader {
 
     public static void main(String[] args) {
         FilePartReader f = new FilePartReader();
-        f.setup("test2.txt", 1, 2);
-        System.out.println(f.read());
+        f.setup("test2.txt", 1, 3);
+        System.out.println(f.readLines());
     }
 
     public void setup (String filePath, Integer fromLine, Integer toLine){
-        this.filePath = filePath;
-        this.fromLine = fromLine;
-        this.toLine = toLine;
-
+        if(fromLine < 1){
+            throw new IllegalArgumentException();
+        }
+        else if (toLine < fromLine){
+            throw new IllegalArgumentException();
+        }
+        else {
+            this.filePath = filePath;
+            this.fromLine = fromLine-1;
+            this.toLine = toLine-1;
+        }
     }
 
     public String readLines (){
         String allContent = read();
         String[] lines = allContent.split( "\n" );
-        ArrayList<String> linesList = new ArrayList<String>(Arrays.asList(lines));
-        int lineIndex = 0;
-        Iterator iterator = linesList.iterator();
-        while (iterator.hasNext()){
-             //ponumerowac line
+        Map<Integer, String> mapWithLines = new HashMap<>();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i<lines.length; i++){
+            mapWithLines.put(i, lines[i]);
         }
-        return "";
+        for (int j = fromLine; j<=toLine; j++){
+            result.append(mapWithLines.get(j)+System.lineSeparator());
+        }
+        return result.toString();
     }
 
     public String read () {
@@ -47,7 +54,6 @@ public class FilePartReader {
             File file = new File(filePath);
             BufferedReader br = new BufferedReader(new FileReader(file));
             String st;
-
             while ((st = br.readLine()) != null)
                 allContent.append(st + System.lineSeparator());
         }catch (IOException exc){
@@ -56,8 +62,4 @@ public class FilePartReader {
         return allContent.toString();
     }
 
-    //should I make seeter or set alle variables in setup method?
-//    public void setFilePath(String filePath) {
-//        this.filePath = filePath;
-//    }
 }
